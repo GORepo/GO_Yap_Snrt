bfae9edd0265deeaded963900e4599c9c7b35a9a
GORepo <azizd@gelistirmeofisi.com>
2010-12-05 18:45:32 +0200

    smarty insert

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+ require_once $_SERVER['DOCUMENT_ROOT'] . '/libs.inc.php';
+
+?>
+safas
--- libs.inc.php
+++ libs.inc.php
@@ -1,15 +1,8 @@
 <?php
-
 $fixpath = $_SERVER['DOCUMENT_ROOT'];
-
 define ("SMARTY_DIR", $_SERVER['DOCUMENT_ROOT']."/lib");
-
-require_once (SMARTY_DIR."Smarty.class.php");
+require_once (SMARTY_DIR."/smarty/Smarty.class.php");
 $smarty = new Smarty;
 $smarty->compile_dir = "$fixpath/compile";
 $smarty->template_dir = "$fixpath/html";
-
-
-
-
 ?>
